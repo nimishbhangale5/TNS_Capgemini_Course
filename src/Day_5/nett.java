bdfea9369d3945b197b99a403b5d32ac95180aed
Nimish Bhangale <91449680+nimishbhangale5@users.noreply.github.com>
2023-02-13 14:04:52 +0530

    Add files via upload

---
+++ src/Day_5/nett.java
@@ -0,0 +1,34 @@
+package Day_5;
+
+public class nett {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		nett f = new nett();
+		f.setrollno(22);
+		f.setrollno(67);
+		f.setrollno(92);
+		f.setrollno(523);
+		f.setrollno(69);
+		f.setrollno(432);
+		f.setrollno(91);
+		f.setrollno(79);
+		
+
+	}
+
+	private int rollno;
+
+	public void setrollno(int x) {
+		// TODO Auto-generated method stub
+		this.rollno=x;
+		
+	}
+	
+	public int getrollno() {
+		// TODO Auto-generated method stub
+		return rollno;
+		
+	}
+
+}
---
+++ src/Day_5/variables.java
@@ -0,0 +1,20 @@
+package Day_5;
+
+public class variables {
+	
+	int r=23;
+	static int g=25;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int f=28;;
+		System.out.println(f);
+		
+		System.out.println(g);
+		
+		variables test = new variables();
+		System.out.println(test.r);
+
+	}
+
+}
